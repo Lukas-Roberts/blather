732c7f2c68e49739743dcaea08c447a799b0aeee
Garth Lilly <garff247@gmail.com>
2020-07-07 12:37:38 -0700

    added email and phone number validations to user

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-    validates :username, presence: true, uniqueness: true, on: :create
+    validates :username, :email, :phone_number, presence: true, uniqueness: true, on: :create
     validates :first_name, :last_name, presence: true
+    
 end
