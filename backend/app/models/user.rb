98d89c8edf83c76d462d190d6f2680a6e2eb732a
Garth Lilly <garff247@gmail.com>
2020-07-07 12:54:15 -0700

    added has many bleats to user

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
     validates :first_name, :last_name, presence: true
     validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
     has_secure_password
+
+    has_many :bleats
     
 end
