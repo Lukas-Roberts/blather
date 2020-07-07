c5d3aec80f48fab62e1036e3bf519142dc1148bf
Garth Lilly <garff247@gmail.com>
2020-07-07 12:43:43 -0700

    added email validations to user

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true, on: :create
     validates :first_name, :last_name, presence: true
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
     has_secure_password
     
 end
