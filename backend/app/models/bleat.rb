d6266f4121f9dc32fe26bd8c8db32b3fe6b7964e
Garth Lilly <garff247@gmail.com>
2020-07-07 13:08:49 -0700

    added belongs_to association to bleats

--- backend/app/models/bleat.rb
+++ backend/app/models/bleat.rb
@@ -1,3 +1,4 @@
 class Bleat < ApplicationRecord
     validates :content, presence: true, length: { maximum: 240, too_long: '240 characters is the maximum allowed'}
+    belongs_to :user
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -3,7 +3,5 @@ class User < ApplicationRecord
     validates :first_name, :last_name, presence: true
     validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
     has_secure_password
-
     has_many :bleats
-    
 end
