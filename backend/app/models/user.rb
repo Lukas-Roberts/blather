33ee32b1da94d898ef998b386fe77463e757fcd2
Garth Lilly <garff247@gmail.com>
2020-07-07 12:40:21 -0700

    added 'has_secure_password' to users

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     validates :username, :email, :phone_number, presence: true, uniqueness: true, on: :create
     validates :first_name, :last_name, presence: true
+    has_secure_password
     
 end
