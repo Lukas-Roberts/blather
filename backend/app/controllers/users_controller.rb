a5f120213e4d18d454e431a235955610128d0d6a
Garth Lilly <garff247@gmail.com>
2020-07-08 14:21:46 -0700

    added create method to users controller

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+
+    def create
+        @user = User.create(username: params[:username], password: params[:password], password_confirmation: params[:password_confirmation], email: prarms[:email], first_name: params[:first_name], last_name: prarms[:last_name])
+        if params[:phone_number]
+            @user.phone_number = params[:phone_number]
+        end
+        render json: @user
+    end
+
 end
