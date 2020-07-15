c017f344bb59d4b4c24cde1108bc833cf4a1f2dc
Garth Lilly <garff247@gmail.com>
2020-07-15 12:40:27 -0700

    stubbed out create session method

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
 
-    
+    def new
+    end
+
+    def create
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            #log in user
+            #render json & pass user
+        else
+            #render json error
+        end
+    end
+
 end
