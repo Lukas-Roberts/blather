9d6f78ab045719f6430bcf1a3e559e5b2fb94b2f
Garth Lilly <garff247@gmail.com>
2020-07-28 14:57:53 -0700

    added handleSubmit event to loginform

--- frontend/src/components/Login/LoginForm.js
+++ frontend/src/components/Login/LoginForm.js
@@ -12,6 +12,19 @@ export default class LoginForm extends Component {
             [event.target.name]: event.target.value
         })
 
-        
+        handleSubmit = event {
+            event.preventDefault()
+            fetch('http://localhost:3001/sessions', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(this.state)
+            })
+                .then(resp => resp.json())
+                .then(json => console.log(json))
+                .catch(error => console.log(error))
+        }
     }
 }
