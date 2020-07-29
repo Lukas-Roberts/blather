5e6f55b966e75054cda6ec172f0a1941ef4773be
Garth Lilly <garff247@gmail.com>
2020-07-29 14:02:24 -0700

    added styles to HomeContainer.css

--- frontend/src/containers/HomeContainer.js
+++ frontend/src/containers/HomeContainer.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import SignupForm from '../components/Signup/SignupForm'
-import LoginForm from '../components/Login/LoginForm'
+import './HomeContainer.css'
 
 const HomeContainer = () => (
-    <div>
+    <div className='home'>
         <h2>Welcome to Blather!</h2>
         <h3>To Start Bleating</h3>
         <p><Link to='/login'>Login</Link> or <Link to='/signup'>Signup</Link></p>
