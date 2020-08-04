86e06b6772f44006d173c7c72d19782227ec26a9
Garth Lilly <garff247@gmail.com>
2020-08-04 15:28:15 -0700

    defined getFeed method for users

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,18 +1,18 @@
 class User < ApplicationRecord
     has_secure_password
-
     validates :username, presence: true, uniqueness: true, on: :create
     validates :password, length: { in: 8..20 }, on: :create
     validates :password_confirmation, presence: true, on: :create
     validates :first_name, :last_name, presence: true
     validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-    
     has_many :bleats, dependent: :destroy
-    
     has_many :following_users, foreign_key: "follower_id", dependent: :destroy
     has_many :follower_users, class_name: "FollowingUser", foreign_key: "following_id", dependent: :destroy
     has_many :followers, class_name: "User", through: :follower_users, foreign_key: "follower_id"
     has_many :following, class_name: "User", through: :following_users, foreign_key: "following_id"
-    attr_accessor :feed
+
+    def getFeed
+
+    end
     
 end
