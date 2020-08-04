bfc953bb6c212bf4b1337c3b2df9bdafb8206605
Garth Lilly <garff247@gmail.com>
2020-08-04 13:06:22 -0700

    added actions to action index

---
+++ frontend/src/actionTypes/index.js
@@ -0,0 +1,16 @@
+const ADD_USER = 'ADD_USER'
+const SET_USER = 'SET_USER'
+
+const ADD_BLEAT = 'ADD_BLEAT'
+
+const FOLLOW_USER = 'FOLLOW_USER'
+const UNFOLLOW_USER = 'UNFOLLOW_USER'
+
+export {
+    ADD_USER,
+    SET_USER,
+    ADD_BLEAT,
+    FOLLOW_USER,
+    UNFOLLOW_USER
+}
+
