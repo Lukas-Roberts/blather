5ff7f722846780be1317ceced72864da13515d60
Garth Lilly <garff247@gmail.com>
2020-08-13 12:04:45 -0700

    added bleat input to homecontainer

--- frontend/src/containers/HomeContainer.js
+++ frontend/src/containers/HomeContainer.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './HomeContainer.css';
+import BleatInput from '../components/Bleats/BleatInput';
 
 const HomeContainer = () => (
     <div className='home'>
-        
+        <BleatInput />
     </div>
 )
