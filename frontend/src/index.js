1c1456887b0f13f17241fe08a64fb821c2c71e03
Garth Lilly <garff247@gmail.com>
2020-08-13 16:28:20 -0700

    created rootReducer and loginReducer files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import rootReducer from './reducers/rootReducer.js'
 import './index.css';
 import App from './App';
 
