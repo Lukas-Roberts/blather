9e0a69dfd2ddead83b22c7d2471e12f71ebd0321
Garth Lilly <garff247@gmail.com>
2020-08-13 16:38:48 -0700

    added setUser and clearUser actions to userActions

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,3 +3,15 @@ import {
     CLEAR_USER
 } from '../actionTypes/index'
 
+export function setUser(userObj) {
+    return {
+        type: SET_USER,
+        payload, userObj
+    }
+}
+
+export function clearUser() {
+    return {
+        type: CLEAR_USER
+    }
+}
