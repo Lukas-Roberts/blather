fdd5ca7451c2f11d585b7023d5a43a421050b7c0
Garth Lilly <garff247@gmail.com>
2020-08-17 15:16:41 -0700

    added sheep logo

--- frontend/src/containers/LandingContainer.js
+++ frontend/src/containers/LandingContainer.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import '../css/LandingContainer.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../css/LandingContainer.css';
+import sheep from '../sheep.png';
 
 const LandingContainer = () => (
     <div className='landing'>
-        <h2>Welcome to Blather!</h2>
-        <h3>To Start Bleating</h3>
-        <p><Link to='/login'>Login</Link> or <Link to='/signup'>Signup</Link></p>
+        <img src={sheep} alt='sheep' className='logo' />
+        <h3>Join Blather today.</h3>
+        <p><Link to='/login'><button>Login</button></Link><br />
+        <Link to='/signup'><button>Signup</button></Link></p>
     </div>
 )
 
