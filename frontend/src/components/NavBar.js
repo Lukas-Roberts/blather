b7c76b441753539dc64ba577b576dfb0369ee351
Garth Lilly <garff247@gmail.com>
2020-08-17 16:57:48 -0700

    added sheep img and links to navlink

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import sheep from '../sheep.png'
 
-const NavBar = () => {
+const NavBar = (props) => {
     return (
         <div className='navbar'>
-            <ul>
-                <li><NavLink to='/'>Home</NavLink></li>
-                <li><NavLink to='/login'>Login</NavLink></li>
-                <li><NavLink to='/home'>logged in home</NavLink></li>
-            </ul>
+            {props.loggedIn ? 
+                <ul>
+                    <NavLink to='/home'><img src={sheep} alt='sheep' /></NavLink>
+                    <NavLink to='/home'>logged in home</NavLink>
+                </ul>
+                :
+                <ul>
+                    <NavLink to='/'><img src={sheep} alt='sheep' /></NavLink>
+                    <NavLink to='/login'>Login</NavLink>
+                </ul>
+            }
         </div>
     )
 }
 
-export default NavBar;
+const mapStateToProps = state => {
+    return {
+        loggedIn: state.loggedIn
+    }
+}
+
+export default connect(mapStateToProps)(NavBar);
